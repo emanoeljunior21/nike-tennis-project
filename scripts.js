a8949ba93415e9cbe8f9f87ced6b01163087c47d
emanoeljunior21 <emanoeljuniordev@gmail.com>
2025-05-28 20:37:04 -0300

    ´projeto-nike

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let body = document.querySelector("body");
+let tenis = document.querySelector(".imagem-tenis");
+let botaoCarrinho = document.querySelector(".botao-carrinho"); 
+
+function mudarVisual(cor, imagem) {
+  tenis.classList.add("troca-efeito");
+
+  body.style.background = cor; 
+  tenis.src = imagem; 
+
+  botaoCarrinho.style.backgroundColor = cor; 
+
+  setTimeout(() => {
+    tenis.src = imagem; 
+    tenis.classList.remove("troca-efeito"); 
+  }, 500);
+}
