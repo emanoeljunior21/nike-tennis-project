bb6ada5bbbb7c0e43ed2bf483120d5a83d22ae31
Emanoel Júnior <emanoeljuniordev@gmail.com>
2025-09-25 20:28:39 -0300

    first commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
 let body = document.querySelector("body");
 let tenis = document.querySelector(".imagem-tenis");
-let botaoCarrinho = document.querySelector(".botao-carrinho"); 
+let botaoCarrinho = document.querySelector(".botao-carrinho");
 
 function mudarVisual(cor, imagem) {
   tenis.classList.add("troca-efeito");
 
-  body.style.background = cor; 
-  tenis.src = imagem; 
+  body.style.background = cor;
+  tenis.src = imagem;
 
-  botaoCarrinho.style.backgroundColor = cor; 
+  botaoCarrinho.style.backgroundColor = cor;
 
   setTimeout(() => {
-    tenis.src = imagem; 
-    tenis.classList.remove("troca-efeito"); 
+    tenis.src = imagem;
+    tenis.classList.remove("troca-efeito");
   }, 500);
 }
+
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
